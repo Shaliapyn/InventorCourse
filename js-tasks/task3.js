d4a89e8d0e91ee9a2a1238905054b2f233da5e29
Anonymous-dev-v <83170790+Anonymous-dev-v@users.noreply.github.com>
2022-04-05 22:45:13 +0300

    Update task3.js

--- js-tasks/task3.js
+++ js-tasks/task3.js
@@ -1,5 +1,5 @@
 const map = (fn, array) => array.map((el) => fn(el))
-
+//here also
 function square(x) { return x * x; } // возведение в квадрат
 
 console.log(map(square, [1, 2, 3, 4])); // [1, 4, 9, 16]
